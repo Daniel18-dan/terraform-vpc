b47fb0ce4bcff2c9a4820806444ce6b6bd3fcb40
Daniel18-dan <bernarddaniel171@gmail.com>
2026-01-21 03:17:36 -0800

    Add NAT Gateway and EIP resources in igw_nat.tf

--- igw_nat.tf
+++ igw_nat.tf
@@ -5,3 +5,17 @@ resource "aws_internet_gateway" "igw" {
     Name = "dan-igw"
   }
 }
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "dan-nat"
+  }
+}
+
